61704cc56d695fd105231c79a2933976bbbcc430
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-23 05:57:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,11 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
   
-  
-  
-  
-  
+  for i in 0..array.length()
+   
+    if array[i] == value_to_find
+      return i
+    end  
+   end
+   return nil
 end
